5fab568f81c65af5e31d27c08090497d164f34b6
Ranya Ouni <ranya.ouni.7@gmail.com>
2023-06-23 18:45:10 +0100

    practicejs

---
+++ Week1/Day 5/Practice/LoopChallenges.js
@@ -0,0 +1,52 @@
+ //Print Odds 1-20
+/*for (var i=1;i<=20;i++){
+    if(i%2==0){
+        console.log(i);
+     }
+ }*/
+ //Print Odds 1-20 second method
+/*var i = 0;
+ while(i<=20){
+    if (i%2==0){
+        console.log(i);
+    }
+    i=i+1;
+ }*/
+
+
+//Decreasing multiples of 3
+
+/*for (var i=100;i>=0;i-=3){
+    if (i%3==0){
+        console.log(i);
+    }
+}*/
+
+//Decreasing multiples of 3 second method
+/*var i=100;
+while(i>=0){
+    if(i%3==0){
+        console.log(i);
+    }
+    i=i-1;
+}*/
+
+//Print of sequence
+/*var array=[4,2.5,1,-0.5,-2,-3.5];
+for (var i=0;i<array.length;i++){
+    console.log(array[i]);
+}*/
+
+//Sigma
+/*var sum=0;
+for (var i=1;i<=100;i++){
+sum=sum+i;
+}
+console.log(sum);*/
+
+//Factorial
+/*var fac=1;
+for (i=2;i<=12;i++){
+    fac=fac*i;
+}
+console.log(fac);*/
